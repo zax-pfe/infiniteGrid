6e65d0790a1b0435585b522938f1b50b0c6e63ca
zax-pfe <axel75puech@gmail.com>
2026-06-10 23:14:27 +0200

    element frise working almost, behavior to debug

--- src/Components/Exhibition/ExhibitionModal/ExhibitionModal.jsx
+++ src/Components/Exhibition/ExhibitionModal/ExhibitionModal.jsx
@@ -12,6 +12,9 @@ const CONFIG = {
   DISPLAYED_ELEMENTS: 3,
   ADDED_ELEMENTS: 2,
   LERP: 0.08,
+  GAP: 300,
+  EXTRA_GAP: 150,
+  MAX_SCALE: 1.8,
 };
 
 export default function ExhibitionModal({ exhibition }) {
@@ -30,6 +33,11 @@ export default function ExhibitionModal({ exhibition }) {
   const pageWidth = useRef(0);
   const pageCenter = useRef(0);
   const positions = useRef([]);
+  const targetPositions = useRef([]);
+  const elementID = useRef([]);
+  const scaleRef = useRef([]);
+  const currentShift = useRef(0);
+
   const gap = useRef(0);
 
   const canvasDimmensions = useRef(0);
@@ -39,12 +47,37 @@ export default function ExhibitionModal({ exhibition }) {
     const updateLayout = () => {
       pageWidth.current = window.innerWidth;
       pageCenter.current = pageWidth.current / 2;
-      gap.current = pageWidth.current / CONFIG.DISPLAYED_ELEMENTS;
-      canvasDimmensions.current = gap.current * exhibitionExtended.length;
-      positions.current = Array.from({ length: exhibitionExtended.length }, (_, i) => ({
-        x: i * gap.current,
-        // x: i * gap.current + pageCenter.current - canvasDimmensions.current / 2,
-      }));
+
+      gap.current = CONFIG.GAP;
+
+      canvasDimmensions.current = gap.current * exhibitionExtended.length + CONFIG.EXTRA_GAP * 2;
+      console.log("canvasDimmensions", canvasDimmensions.current);
+
+      // positions.current = Array.from({ length: exhibitionExtended.length }, (_, i) => ({
+      //   x:
+      //     i === 3
+      //       ? i * gap.current + gap.current + gap.current / 2
+      //       : i > 3
+      //         ? i * gap.current + 2 * gap.current + gap.current / 2
+      //         : i * gap.current + gap.current / 2,
+      // }));
+      positions.current = [
+        { x: -1050, id: 0 },
+        { x: -750, id: 1 },
+        { x: -450, id: 2 },
+        { x: 0, id: 3 },
+        { x: 450, id: 4 },
+        { x: 750, id: 5 },
+        { x: 1050, id: 6 },
+      ];
+
+      targetPositions.current = positions.current.map((pos) => ({ ...pos, x: pos.x }));
+      console.log("initial positions", positions.current);
+      console.log("initial positions with targetX", targetPositions.current);
+
+      scaleRef.current = Array.from({ length: exhibitionExtended.length }, () => ({ value: 1 }));
+
+      console.log("positions", positions.current);
     };
 
     updateLayout();
@@ -65,16 +98,30 @@ export default function ExhibitionModal({ exhibition }) {
       if (canvasDimmensions.current === 0) return;
       for (const [index, el] of elementRef.current.entries()) {
         const pos = positions.current[index];
+        const scale = scaleRef.current[index];
+
         if (!el || !pos) continue;
 
         pos.x = lerp(pos.x, pos.targetX ?? pos.x, CONFIG.LERP);
-        pos.y = lerp(pos.y, pos.targetY ?? pos.y, CONFIG.LERP);
+        // pos.y = lerp(pos.y, pos.targetY ?? pos.y, CONFIG.LERP);
 
         const moduloX =
           mod(pos.x + canvasDimmensions.current / 2, canvasDimmensions.current) -
           canvasDimmensions.current / 2;
 
-        gsap.set(el, { x: moduloX + pageCenter.current, y: 400 });
+        if (moduloX > -200 && moduloX < 200) {
+          // console.log("visible", index);
+          scale.value = lerp(scale.value, CONFIG.MAX_SCALE, CONFIG.LERP);
+        } else {
+          scale.value = lerp(scale.value, 1, CONFIG.LERP);
+        }
+
+        gsap.set(el, {
+          x: moduloX + pageCenter.current,
+          y: 400,
+          xPercent: -50,
+          scale: scale.value,
+        });
       }
 
       requestRef.current = requestAnimationFrame(animate);
@@ -85,22 +132,59 @@ export default function ExhibitionModal({ exhibition }) {
     return () => cancelAnimationFrame(requestRef.current);
   }, []);
 
+  function shortestWrappedTarget(current, target, width) {
+    // Calculate the shortest distance between current and target, considering wrapping
+    // This ensures that the animation takes the shortest path, even if it means wrapping around the edges
+    // For example, if current is near the right edge and target is near the left edge, it will wrap around instead of going all the way across
+    let delta = target - current;
+
+    if (delta > width / 2) {
+      target -= width;
+    } else if (delta < -width / 2) {
+      target += width;
+    }
+
+    return target;
+  }
+
   const handleClick = (index) => {
     const pos = positions.current[index];
+    let isRightClick = false;
 
-    // Position visuelle de l'élément (même calcul que dans le RAF)
+    if (pos.id === 3) return;
+
+    if (pos.id > 3) {
+      currentShift.current = currentShift.current - 1;
+      isRightClick = true;
+    }
+
+    if (pos.id < 3) {
+      currentShift.current = currentShift.current + 1;
+      isRightClick = false;
+    }
+
     const visualX =
       mod(pos.x + canvasDimmensions.current / 2, canvasDimmensions.current) -
       canvasDimmensions.current / 2;
 
-    // On veut que visualX + pageCenter soit égal à pageCenter (le centre de l'écran)
-    // Donc diff = -visualX
-    const diff = -visualX;
+    console.log("clicked element id", index);
 
     for (let i = 0; i < positions.current.length; i++) {
       const temp = positions.current[i];
-      temp.targetX = (temp.targetX ?? temp.x) + diff;
+
+      temp.id = mod(temp.id + (isRightClick ? -1 : 1), exhibitionExtended.length);
+
+      let targetIndex = mod(i + currentShift.current, exhibitionExtended.length);
+
+      let targetPosition = targetPositions.current[targetIndex];
+
+      temp.targetX = shortestWrappedTarget(temp.x, targetPosition.x, canvasDimmensions.current);
+
+      console.log("target index", targetIndex);
+      console.log("target position", targetPosition);
     }
+
+    console.log("positions after click", positions.current);
   };
 
   return (
